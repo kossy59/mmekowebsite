692610349853c2de82c266a14a187a8a42967298
Mintlord3ttah <lordmint7@gmail.com>
2025-07-19 17:10:38 +0100

    ui responsive

--- src/app/auth/_components/Layout.tsx
+++ src/app/auth/_components/Layout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div className='grid grid-cols-[60fr_40fr] min-h-screen w-full'>
-        <div className='bg-img bg-slate-500 flex-1'></div>
+    <div className='grid grid-cols-[60fr_40fr] max-sm:grid-cols-1 w-full'>
+        <div className='bg-img w-full max-sm:w-0 max-sm:h-0 transition-all duration-500 h-screen'></div>
         {children}
     </div>
   )
