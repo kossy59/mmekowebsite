44545cb13ef37799f2548be8d052851217019102
Mintlord3ttah <lordmint7@gmail.com>
2025-07-21 15:24:42 +0100

    add metadata

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Register } from './RegisterComponent'
 
+export const metadata = {
+  title: "Register",
+  description: "Create your account here",
+}
 export default function page() {
   return <Register />
 }
